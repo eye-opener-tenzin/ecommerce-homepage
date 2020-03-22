a91c3317f47f68f782e58ec1ffc102cd15463b37
Stenzin Mejay <47583037+Tenzin-Mejay@users.noreply.github.com>
2020-03-22 10:00:57 -0700

    Updates

---
+++ precache-manifest.ae92a42e8fd623278bdb0397a954b55d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c5be87090a3caac6356c5cfef5f9c9",
+    "url": "/eye-opener-tenzin/ecommerce.git/index.html"
+  },
+  {
+    "revision": "691b3fb88ff6099c7bc8",
+    "url": "/eye-opener-tenzin/ecommerce.git/static/css/main.3d6cb93d.chunk.css"
+  },
+  {
+    "revision": "7d438e52d0c5cf2b21ba",
+    "url": "/eye-opener-tenzin/ecommerce.git/static/js/2.7aa7f027.chunk.js"
+  },
+  {
+    "revision": "f24374ea0f456cc9443148e7c43a14cc",
+    "url": "/eye-opener-tenzin/ecommerce.git/static/js/2.7aa7f027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "691b3fb88ff6099c7bc8",
+    "url": "/eye-opener-tenzin/ecommerce.git/static/js/main.d8be9074.chunk.js"
+  },
+  {
+    "revision": "d5e46d26eae1150883bf",
+    "url": "/eye-opener-tenzin/ecommerce.git/static/js/runtime-main.dcc1c614.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/eye-opener-tenzin/ecommerce.git/static/media/crown.ed28accb.svg"
+  }
+]);
